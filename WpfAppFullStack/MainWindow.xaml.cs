96e9f05ff5601cce58254e87b29c09a7be8385c5
rknie <rkni0004@student.monash.edu>
2023-11-12 18:26:27 +1100

    Add project files.

---
+++ WpfAppFullStack/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace WpfAppFullStack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string conncetionString = ConfigurationManager.ConnectionStrings["WpfAppFullStack.Properties.Settings.C_FullStackConnectionString"].ConnectionString;
+        }
+    }
+}
